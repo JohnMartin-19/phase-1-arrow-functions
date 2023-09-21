12e3cf1d297b8386a0106f6d6be1b10a8bcc5389
John Mburu <johnnkonge2020@gmail.com>
2023-09-21 20:01:42 +0300

    completed

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide = num => {
+    result = num/100;
+    console.log(result)
+}
+divide(2000)
+
+const square = x => {
+    sq = x*x;
+    console.log(sq)
+}
+square(2)
+
+const add = (num1,num2) => {
+    addition = num1+num2;
+    console.log(addition)
+}
+add(3,4)
